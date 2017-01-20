248df2c9d6779653ac56bb358f0681ecf0046ec6
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-20 08:21:45 -0800

    add comments about the specs of program on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
 $(function(){
 
+//BACK LOGIC GOES HERE
+
+
+  //STEP1: given userInput, check if it is a number
+
+
+
+  //STEP2: given Usernumber, output the array
+
+  //STEP3: given the array, replace the specific numbers of array to according words;
+  //using for-loop and when if you found either 15 or 5 or 3, replace
+    //[1, 2, ping, 4, pong, ping, 7, 8, ping, pong,.....14, ping-pong]
+
+  //STEP4: Display the Array (using for-loop)
+
+
+//FRONT LOGIC GOES HERE
+
+  //Implement tryagain (previous output disappeared and user can try again)
+
+  //get the userInput
+
+  //call func STEP1
+
+  //call func STEP2
+
+  //call func STEP3
+
+  //Call func Step4 Display:: show the result of the array
+  //"<li>" + "</li>""
+
+  ///If I finished early,
+  //show number and ping-pong words, gradually!!
+  //ping-pong, animation and color!!
 });
